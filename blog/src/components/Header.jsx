1a90d80c3e0d10aa2c813abd1ba1f09094f3569d
Ron Crouse <rcrouse927@gmail.com>
2024-08-11 10:25:57 -0400

    setting up website

---
+++ blog/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './../assets/Images/logo.png';
+
+function Header() {
+  return (
+    <div>
+        <img src= {logo} />
+    </div>
+  )
+}
+
+export default Header
---
+++ blog/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header';
+import Search from '../components/Search';
+import IntroPost from '../components/IntroPost';
+import Blogs from '../components/Blogs';
+import Footer from '../components/Footer';
+
+
+function Home() {
+  return (
+    <div>
+        <Header />
+        <Search />
+        <IntroPost />
+        <Blogs />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
